3ae06655e360f5d5db0375ea234e03d8f17ea231
Saatvic Mahesh <saatvicmahesh@Saatvics-MBP.lan>
2026-08-24 18:19:01 -0700

    Add Committees and Fundraising features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
+import Committees from './Committees';
+import Fundraising from './Fundraising';
 
 const api = axios.create({
   baseURL: '/api',
